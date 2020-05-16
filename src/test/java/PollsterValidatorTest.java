8a2a1ae66916ec0266bbaf17c08ee39f770537de
Sim-Pirs <43365681+Sim-Pirs@users.noreply.github.com>
2020-05-16 16:59:22 +0200

    Validator testsss

---
+++ src/test/java/PollsterValidatorTest.java
@@ -0,0 +1,106 @@
+import org.junit.jupiter.api.Test;
+import sondage.entity.model.*;
+import static org.junit.jupiter.api.Assertions.*;
+import java.lang.reflect.Field;
+import javax.validation.constraints.*;
+
+public class PollsterValidatorTest {
+	
+	/*Partie 1 test des regex*/
+	
+	
+		/*Méthodes de test*/
+		public void firstNameRegex(String firstName,boolean validates) throws NoSuchFieldException, SecurityException{
+		    Field field = Pollster.class.getDeclaredField("firstName");
+		    Pattern[] annotations = field.getAnnotationsByType(Pattern.class);    
+		    assertEquals(firstName.matches(annotations[0].regexp()), validates);
+		}
+		
+		public void lastNameRegex(String lastName, boolean validates) throws NoSuchFieldException, SecurityException{
+			Field field = Pollster.class.getDeclaredField("lastName");
+		    Pattern[] annotations = field.getAnnotationsByType(Pattern.class);
+		    assertEquals(lastName.matches(annotations[0].regexp()), validates);
+		}
+		
+		public void emailRegex(String email, boolean validates) throws NoSuchFieldException, SecurityException{
+			Field field = Pollster.class.getDeclaredField("email");
+			Pattern[] annotations = field.getAnnotationsByType(Pattern.class);
+			assertEquals(email.matches(annotations[0].regexp()), validates);
+		}
+		
+		public void passwordRegex(String password, boolean validates) throws NoSuchFieldException, SecurityException{
+			Field field = Pollster.class.getDeclaredField("password");
+			Pattern[] annotations = field.getAnnotationsByType(Pattern.class);
+			assertEquals(password.matches(annotations[0].regexp()), validates);
+		}
+		
+		
+		/*Tests FirstName*/
+		@Test
+		public void testPollsterBadFirstName() throws NoSuchFieldException, SecurityException {
+			Pollster pollster = new Pollster();
+			pollster.setFirstName("51m0n");
+			firstNameRegex(pollster.getFirstName(), false);
+		}
+		
+		@Test
+		public void testPollsterGoodFirstName() throws NoSuchFieldException, SecurityException {
+			Pollster pollster = new Pollster();
+			pollster.setFirstName("Simon");
+			firstNameRegex(pollster.getFirstName(), true);
+		}
+		/*******************************************************************************************************/
+		/*Tests LastName*/
+		@Test
+		public void testPollsterBadLastName() throws NoSuchFieldException, SecurityException {
+			Pollster pollster = new Pollster();
+			pollster.setLastName("P13R35");
+			lastNameRegex(pollster.getLastName(), false);
+		}
+		
+		@Test
+		public void testPollsterGoodLastName() throws NoSuchFieldException, SecurityException {
+			Pollster pollster = new Pollster();
+			pollster.setLastName("PIERES");
+			lastNameRegex(pollster.getLastName(), true);
+		}
+		/*******************************************************************************************************/
+		/*Tests Email*/
+		@Test
+		public void testPollsterBadEmail() throws NoSuchFieldException, SecurityException{
+			Pollster pollster = new Pollster();
+			pollster.setEmail("simon.com");
+			emailRegex(pollster.getEmail(), false);
+		}
+		
+		public void testPollsterGoodEmail() throws NoSuchFieldException, SecurityException{
+			Pollster pollster = new Pollster();
+			pollster.setEmail("simon@gmail.com");
+			emailRegex(pollster.getEmail(), true);
+		}
+		
+		/*******************************************************************************************************/
+		/*Tests Password*/
+	/*	@Test
+		public void testPollsterBadPasswordLess8Characters() throws NoSuchFieldException, SecurityException{
+			Pollster pollster = new Pollster();
+			pollster.setPassword("1234");
+			passwordRegex(pollster.getPassword(), false);
+		}
+		
+		@Test
+		public void testPollsterBadPasswordMore15Characters() throws NoSuchFieldException, SecurityException{
+			Pollster pollster = new Pollster();
+			pollster.setPassword("AZERTYUIOP18903748"); //à changer en fonction du nombre max de caractères autorisés.
+			passwordRegex(pollster.getPassword(), false);
+		}
+		
+		public void testPollsterGoodPassword() throws NoSuchFieldException, SecurityException{
+			Pollster pollster = new Pollster();
+			pollster.setPassword("m4st3rILDgog+");
+			passwordRegex(pollster.getPassword(), true);
+		}*/
+	/*******************************************************************************************************/
+	/*Partie 2 test sur le pollsterValidator*/
+
+}
---
+++ src/test/java/RespondentValidatorTest.java
@@ -0,0 +1,91 @@
+import org.junit.jupiter.api.Test;
+import sondage.entity.model.*;
+import static org.junit.jupiter.api.Assertions.*;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.validation.constraints.*;
+
+public class RespondentValidatorTest {
+	
+	/*Partie 1 test des regex*/
+		/*Méthodes test regex*/
+	
+		public void emailRegex(String email,boolean validates) throws NoSuchFieldException, SecurityException{
+		    Field field = Respondent.class.getDeclaredField("email");
+		    Pattern[] annotations = field.getAnnotationsByType(Pattern.class);    
+		    assertEquals(email.matches(annotations[0].regexp()), validates);
+		}
+		
+		/* probleme : j'arrive pas à recup la regex pour le moment */
+		/*public void tagsRegex(Collection<String> tags,boolean validates) throws NoSuchFieldException, SecurityException{
+		    Field field = Respondent.class.getDeclaredField("tags");
+		    Pattern[] annotations = field.getAnnotationsByType(Pattern.class);
+		    System.out.println("annotations la "+ annotations);
+		    Object[]tagsTable = tags.toArray();
+		    for(int i=0; i<tagsTable.length; i++) {
+		    	System.out.println("i= "+ i + " " + tagsTable[i]);
+		    	assertEquals(((String) tagsTable[i]).matches(annotations[0].regexp()), validates);
+		    }
+		}*/
+		
+		public void tokenRegex(String token,boolean validates) throws NoSuchFieldException, SecurityException{
+		    Field field = Respondent.class.getDeclaredField("token");
+		    Pattern[] annotations = field.getAnnotationsByType(Pattern.class);    
+		    assertEquals(token.matches(annotations[0].regexp()), validates);
+		}
+		
+		/*******************************************************************************************************/
+		/*Tests email*/
+		@Test
+		public void testRespondentBadEmail() throws NoSuchFieldException, SecurityException{
+			Respondent respondent = new Respondent();
+			respondent.setEmail("nomis@truc@truc.com");
+			emailRegex(respondent.getEmail(), false);
+		}
+		
+		@Test
+		public void testRespondentGoodEmail() throws NoSuchFieldException, SecurityException{
+			Respondent respondent = new Respondent();
+			respondent.setEmail("simon@hotmail.fr");
+			emailRegex(respondent.getEmail(), true);
+		}
+		/*******************************************************************************************************/
+		/*Tests tag*/
+		/*@Test
+		public void testRespondentBadTag() throws NoSuchFieldException, SecurityException{
+			Respondent respondent = new Respondent();
+			Collection<String> collection = new ArrayList<String>();
+			collection.add("ILD-11;");
+			collection.add("IAAA--");
+			respondent.setTags(collection);
+			tagsRegex(respondent.getTags(), false);
+		}
+		
+		@Test
+		public void testRespondentGoodTag() throws NoSuchFieldException, SecurityException{
+			Respondent respondent = new Respondent();
+			Collection<String> collection = new ArrayList<String>();
+			collection.add("ILD-1;");
+			collection.add("IAAA-5");
+			respondent.setTags(collection);
+			tagsRegex(respondent.getTags(), true);
+		}*/
+		/*******************************************************************************************************/
+		/*Tests token*/
+		@Test
+		public void testRespondentBadToken() throws NoSuchFieldException, SecurityException{
+			Respondent respondent = new Respondent();
+			respondent.setToken("i05?");
+			tokenRegex(respondent.getToken(), false);
+		}
+		
+		@Test
+		public void testRespondentGoodToken() throws NoSuchFieldException, SecurityException{
+			Respondent respondent = new Respondent();
+			respondent.setToken("ILD1");
+			tokenRegex(respondent.getToken(), true);
+		}
+
+}
