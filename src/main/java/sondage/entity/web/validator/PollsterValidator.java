102538869ec5661c64fc8a4da8d6a6f675d837f2
Romain Colonna D'Istria <romain.colonna-distria@etu.univ-amu.fr>
2020-05-22 20:14:48 +0200

    ajout validation des mots de passes

--- src/main/java/sondage/entity/web/validator/PollsterValidator.java
+++ src/main/java/sondage/entity/web/validator/PollsterValidator.java
@@ -46,6 +46,33 @@ public class PollsterValidator implements Validator {
             errors.rejectValue("email", "pollster.email.invalid");
         }
 
+        boolean containUpper = false;
+        boolean containLower = false;
+        boolean containDigit = false;
+        boolean containSpecial = false;
+
+        if (pollster.getPassword().length() < 8 || pollster.getPassword().length() > 254) {
+            errors.rejectValue("password", "pollster.password.badSize");
+        }
+
+        for (int i = 0; i < pollster.getPassword().length(); i++){
+            char c = pollster.getPassword().charAt(i);
+
+            if (!Character.isDigit(c) && !Character.isLetter(c) && c != ' ') {
+                containSpecial = true;
+            } else if (Character.isDigit(c)) {
+                containDigit = true;
+            } else if (Character.isUpperCase(c)) {
+                containUpper = true;
+            } else if (Character.isLowerCase(c)) {
+                containLower = true;
+            }
+        }
+
+        if(!containDigit || !containLower || !containUpper || !containSpecial){
+            errors.rejectValue("password", "pollster.password.lackCarac");
+        }
+
     }
 
 }
