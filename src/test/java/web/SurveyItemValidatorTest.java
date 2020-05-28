7f6e8d8fa31a53d22cb9bc24af5d5e080304993d
Sim-Pirs <43365681+Sim-Pirs@users.noreply.github.com>
2020-05-28 14:28:41 +0200

    Update SurveyItemValidatorTest.java

--- src/test/java/web/SurveyItemValidatorTest.java
+++ src/test/java/web/SurveyItemValidatorTest.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.*;
 
 public class SurveyItemValidatorTest {
 	
-	/*Partie 1 test des regex*/
+	/*Test des regex*/
 	/*Méthodes test regex*/
 		
 		public void descriptionRegex(String description, boolean validates) throws NoSuchFieldException, SecurityException{
@@ -17,18 +17,6 @@ public class SurveyItemValidatorTest {
 		    assertEquals(description.matches(annotations[0].regexp()), validates);
 		}
 	
-		/* probleme : j'arrive pas à recup la regex pour le moment */
-		/*public void tagsRegex(Collection<String> tags,boolean validates) throws NoSuchFieldException, SecurityException{
-		    Field field = Respondent.class.getDeclaredField("tags");
-		    Pattern[] annotations = field.getAnnotationsByType(Pattern.class);
-		    System.out.println("annotations la "+ annotations);
-		    Object[]tagsTable = tags.toArray();
-		    for(int i=0; i<tagsTable.length; i++) {
-		    	System.out.println("i= "+ i + " " + tagsTable[i]);
-		    	assertEquals(((String) tagsTable[i]).matches(annotations[0].regexp()), validates);
-		    }
-		}*/
-		
 		/*******************************************************************************************************/
 		
 		/*Test Description*/
